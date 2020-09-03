8ea8a20e2b492dbcd601617964ca6ed6e433b929
probably-not-porter <pelibby16@earlham.edu>
2020-09-03 17:57:07 -0400

    create some custom css, add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import Header from './components/Header';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import './css/main.css';
 
 const App: React.FC = () => {
   return (
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Container from 'react-bootstrap/Container';
+import './../css/header.css';
 
-const Header: React.FC = () => {
-
-  return (
-        <Container className="p-3">
-            <Jumbotron>
-                <h1 className="header">
-                    Portfolio Practice Project
-                </h1>
-            </Jumbotron>
-        </Container>
-  );
+class Header extends React.Component{
+    render(){
+        return (
+            <Navbar fixed="top" expand="lg">
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Form inline>
+                        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                        <Button variant="outline-success">Search</Button>
+                    </Form>
+                </Navbar.Collapse>
+            </Navbar>
+        );
+    }
 };
 
 export default Header;

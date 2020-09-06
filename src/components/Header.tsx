5d224bbeaa1008983a1691084f4bb396e7ed026d
probably-not-porter <pelibby16@earlham.edu>
2020-09-06 00:50:51 -0400

    change styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,20 +6,14 @@ import './../css/header.css';
 class Header extends React.Component{
     render(){
         return (
-            <Navbar fixed="top" expand="lg">
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar bg='dark' variant="dark" fixed="top" expand="lg">
+                <Navbar.Brand href="#home">XRP</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="mr-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#link">Forum</Nav.Link>
+                        <Nav.Link href="#discord">Discord</Nav.Link>
                     </Nav>
                     <Form inline>
                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />

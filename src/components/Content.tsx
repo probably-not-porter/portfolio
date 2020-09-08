c8d1657f67cebe2fcc12b553ac1140e5e8fa89d8
probably-not-porter <pelibby16@earlham.edu>
2020-09-08 16:30:37 -0400

    lazy loading images

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
+
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import Carousel from 'react-bootstrap/Carousel'
+import ProgressiveImage from "react-progressive-image-loading";
 
 import './../css/content.css';
 
-import Image1 from './../images/image1-min.png';
-import Image2 from './../images/image2-min.png';
-import Image3 from './../images/image3-min.png';
-import Image4 from './../images/image4-min.png';
-import Image5 from './../images/image5-min.png';
-import Image6 from './../images/image6-min.png';
-import Image7 from './../images/image7-min.png';
-import Image8 from './../images/image8-min.png';
-import Image9 from './../images/image9-min.png';
+// lazy load thumbnails
+import Image1Min from './../images/image1-min.jpg';
+import Image2Min from './../images/image2-min.jpg';
+import Image3Min from './../images/image3-min.jpg';
+import Image4Min from './../images/image4-min.jpg';
+import Image5Min from './../images/image5-min.jpg';
+import Image6Min from './../images/image6-min.jpg';
+import Image7Min from './../images/image7-min.jpg';
+import Image8Min from './../images/image8-min.jpg';
+import Image9Min from './../images/image9-min.jpg';
+
+// full sized images (compressed)
+import Image1 from './../images/image1.png';
+import Image2 from './../images/image2.png';
+import Image3 from './../images/image3.png';
+import Image4 from './../images/image4.png';
+import Image5 from './../images/image5.png';
+import Image6 from './../images/image6.png';
+import Image7 from './../images/image7.png';
+import Image8 from './../images/image8.png';
+import Image9 from './../images/image9.png';
 
 class Content extends React.Component{
     
@@ -20,11 +34,10 @@ class Content extends React.Component{
         return (
             <Carousel interval={5000} pause={false} >
                 <Carousel.Item style={{ height: "100vh" }}>
-                    <img
-                        className="d-block w-100"
+                    <ProgressiveImage
+                        preview={Image1Min}
                         src={Image1}
-                        alt="First slide"
-                        style={{ height: "100vh", objectFit: "cover" }}
+                        render={(src, style) => <img alt="slide image 1" src={src} style={{ height: "100vh", objectFit: "cover" }} />}
                     />
                     <Carousel.Caption>
                         <h3>First slide label</h3>
@@ -32,26 +45,22 @@ class Content extends React.Component{
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item style={{ height: "100vh"}}>
-                    <img
-                        className="d-block w-100"
+                    <ProgressiveImage
+                        preview={Image2Min}
                         src={Image2}
-                        alt="Second slide"
-                        style={{ height: "100vh", objectFit: "cover" }}
+                        render={(src, style) => <img alt="slide image 2" src={src} style={{ height: "100vh", objectFit: "cover" }} />}
                     />
-
                     <Carousel.Caption>
                         <h3>Second slide label</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item style={{ height: "100vh"}}>
-                    <img
-                        className="d-block w-100"
+                    <ProgressiveImage
+                        preview={Image3Min}
                         src={Image3}
-                        alt="Third slide"
-                        style={{ height: "100vh", objectFit: "cover" }}
+                        render={(src, style) => <img alt="slide image 3" src={src} style={{ height: "100vh", objectFit: "cover" }} />}
                     />
-
                     <Carousel.Caption>
                         <h3>Third slide label</h3>
                         <p>
@@ -60,13 +69,11 @@ class Content extends React.Component{
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item style={{ height: "100vh"}}>
-                    <img
-                        className="d-block w-100"
+                    <ProgressiveImage
+                        preview={Image4Min}
                         src={Image4}
-                        alt="Fourth slide"
-                        style={{ height: "100vh", objectFit: "cover" }}
+                        render={(src, style) => <img alt="slide image 4" src={src} style={{ height: "100vh", objectFit: "cover" }} />}
                     />
-
                     <Carousel.Caption>
                         <h3>Fourth slide label</h3>
                         <p>
@@ -75,13 +82,11 @@ class Content extends React.Component{
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item style={{ height: "100vh"}}>
-                    <img
-                        className="d-block w-100"
+                    <ProgressiveImage
+                        preview={Image5Min}
                         src={Image5}
-                        alt="Fifth slide"
-                        style={{ height: "100vh", objectFit: "cover" }}
+                        render={(src, style) => <img alt="slide image 5" src={src} style={{ height: "100vh", objectFit: "cover" }} />}
                     />
-
                     <Carousel.Caption>
                         <h3>Fifth slide label</h3>
                         <p>
@@ -90,13 +95,11 @@ class Content extends React.Component{
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item style={{ height: "100vh"}}>
-                    <img
-                        className="d-block w-100"
+                    <ProgressiveImage
+                        preview={Image6Min}
                         src={Image6}
-                        alt="Sixth slide"
-                        style={{ height: "100vh", objectFit: "cover" }}
+                        render={(src, style) => <img alt="slide image 6" src={src} style={{ height: "100vh", objectFit: "cover" }} />}
                     />
-
                     <Carousel.Caption>
                         <h3>Sixth slide label</h3>
                         <p>
@@ -105,13 +108,11 @@ class Content extends React.Component{
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item style={{ height: "100vh"}}>
-                    <img
-                        className="d-block w-100"
+                    <ProgressiveImage
+                        preview={Image7Min}
                         src={Image7}
-                        alt="Seventh slide"
-                        style={{ height: "100vh", objectFit: "cover" }}
+                        render={(src, style) => <img alt="slide image 7" src={src} style={{ height: "100vh", objectFit: "cover" }} />}
                     />
-
                     <Carousel.Caption>
                         <h3>Seventh slide label</h3>
                         <p>
@@ -120,13 +121,11 @@ class Content extends React.Component{
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item style={{ height: "100vh"}}>
-                    <img
-                        className="d-block w-100"
+                    <ProgressiveImage
+                        preview={Image8Min}
                         src={Image8}
-                        alt="Eighth slide"
-                        style={{ height: "100vh", objectFit: "cover" }}
+                        render={(src, style) => <img alt="slide image 8" src={src} style={{ height: "100vh", objectFit: "cover" }} />}
                     />
-
                     <Carousel.Caption>
                         <h3>Eighth slide label</h3>
                         <p>
@@ -135,13 +134,11 @@ class Content extends React.Component{
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item style={{ height: "100vh"}}>
-                    <img
-                        className="d-block w-100"
+                    <ProgressiveImage
+                        preview={Image9Min}
                         src={Image9}
-                        alt="Ninth slide"
-                        style={{ height: "100vh", objectFit: "cover" }}
+                        render={(src, style) => <img alt="slide image 9" src={src} style={{ height: "100vh", objectFit: "cover" }} />}
                     />
-
                     <Carousel.Caption>
                         <h3>Ninth lide label</h3>
                         <p>

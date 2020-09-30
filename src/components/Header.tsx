5b4175e52da5392b84fdf7bcde115e315e06a0f7
probably-not-porter <pelibby16@earlham.edu>
2020-09-30 02:44:53 -0400

    tooltip nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { Navbar, Nav, Image } from 'react-bootstrap';
+import { Navbar, Nav} from 'react-bootstrap';
 import { FaComments, FaDiscord, FaFacebookF, FaYoutube, FaInstagram } from 'react-icons/fa';
 import {IconContext} from "react-icons"
 
 import './../css/header.css';
+import './../css/tooltip.css';
 
 
 class Header extends React.Component{
@@ -15,22 +16,50 @@ class Header extends React.Component{
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav" style={{minHeight: "50px"}}>
                     <Nav className="mr-auto" style={{minHeight: "50px", lineHeight: "50px", verticalAlign: 'middle', alignContent: "left"}}>
-                        <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-forum", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
-                            <Nav.Link className="link" href=""><FaComments />  Forum</Nav.Link>
-                        </IconContext.Provider>
-                        <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-discord", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
-                            <Nav.Link className="link" href=""><FaDiscord />  Discord</Nav.Link>
-                        </IconContext.Provider>
-                        <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-facebook", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
-                            <Nav.Link className="link" href=""><FaFacebookF />  Facebook</Nav.Link>
-                        </IconContext.Provider>
-                        <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-youtube", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
-                            <Nav.Link className="link" href=""><FaYoutube />  Youtube</Nav.Link>
-                        </IconContext.Provider>
-                        <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-youtube", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
-                            <Nav.Link className="link" href=""><FaInstagram />  Instagram</Nav.Link>
-                        </IconContext.Provider>
-                        
+                        <div className='con-tooltip bottom'>
+                            <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-forum", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
+                                <Nav.Link className="link" href=""><FaComments />  Forum</Nav.Link>
+                            </IconContext.Provider>
+                            <div className="tooltip ">
+                                <span>Forum</span>
+                            </div>
+                        </div>
+
+                        <div className='con-tooltip bottom'>
+                            <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-discord", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
+                                <Nav.Link className="link" href=""><FaDiscord />  Discord</Nav.Link>
+                            </IconContext.Provider>
+                            <div className="tooltip ">
+                                <span>Discord</span>
+                            </div>
+                        </div>
+
+                        <div className='con-tooltip bottom'>
+                            <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-facebook", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
+                                <Nav.Link className="link" href=""><FaFacebookF />  Facebook</Nav.Link>
+                            </IconContext.Provider>
+                            <div className="tooltip ">
+                                <span>Facebook</span>
+                            </div>
+                        </div>
+
+                        <div className='con-tooltip bottom'>
+                            <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-youtube", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
+                                <Nav.Link className="link" href=""><FaYoutube />  Youtube</Nav.Link>
+                            </IconContext.Provider>
+                            <div className="tooltip ">
+                                <span>Youtube</span>
+                            </div>
+                        </div>
+
+                        <div className='con-tooltip bottom'>
+                            <IconContext.Provider value={{ size: "1.5rem", className: "icon icon-youtube", style: { verticalAlign: 'middle', fontSize: "1rem" }  }}>
+                                <Nav.Link className="link" href=""><FaInstagram />  Instagram</Nav.Link>
+                            </IconContext.Provider>
+                            <div className="tooltip ">
+                                <span>Instagram</span>
+                            </div>
+                        </div>
                     </Nav>
                 </Navbar.Collapse>
         </Navbar>

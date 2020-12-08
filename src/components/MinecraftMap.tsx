6a5305a9f7ea3a59cb90640eb1658b880fd4c7a5
probably-not-porter <pelibby16@earlham.edu>
2020-12-08 17:57:40 -0500

    test s

--- src/components/MinecraftMap.tsx
+++ src/components/MinecraftMap.tsx
@@ -7,7 +7,7 @@ class MinecraftMap extends React.Component{
     
     render(){
         return (
-            <Iframe url="http://51.81.80.168:8123/"
+            <Iframe url="https://51.81.80.168:8123/"
                 width="100%"
                 height="100%"
                 id="myId"

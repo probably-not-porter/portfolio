465e4f2ea414c4dae31a2d8b01730ada1052ab14
probably-not-porter <pelibby16@earlham.edu>
2020-12-08 18:02:38 -0500

    reorganize nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,20 +18,19 @@ class Header extends React.Component{
                     <Navbar.Brand style={{WebkitTextStroke: "2px #bbb", color: "black", marginTop: "-20px", marginBottom: "-10px", marginLeft: "20px", marginRight: "50px",fontSize: "3rem", fontFamily: "pricedown"}} href="/">XGC</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="mr-auto" style={{lineHeight: "50px", verticalAlign: 'middle', alignContent: "left"}}>                                
+                            <Nav className="mr-auto" style={{lineHeight: "50px", verticalAlign: 'middle', alignContent: "left"}}>  
+                            
+                                <NavDropdown title="GTA V" id="nav-dropdown">
+                                    <NavDropdown.Item href="/gta-wiki">Wiki</NavDropdown.Item>
+                                    <NavDropdown.Item href="/gta-forum">Forum</NavDropdown.Item>
+                                    <NavDropdown.Item href="/gta-perks">Perk Store</NavDropdown.Item>
+                                </NavDropdown>          
+
                                 <NavDropdown title="Minecraft" id="nav-dropdown">
                                     <NavDropdown.Item href="/mc-wiki">Wiki</NavDropdown.Item>
                                     <NavDropdown.Item href="/mc-map">Map</NavDropdown.Item>
                                     <NavDropdown.Item href="/mc-perks">Perk Store</NavDropdown.Item>
                                 </NavDropdown>
-
-                                <NavDropdown title="GTA V" id="nav-dropdown">
-                                    <NavDropdown.Item href="/gta-wiki">Wiki</NavDropdown.Item>
-                                    <NavDropdown.Item href="/gta-forum">Forum</NavDropdown.Item>
-                                    <NavDropdown.Item href="/gta-perks">Perk Store</NavDropdown.Item>
-                                </NavDropdown>
-                                
-                                
 
                                 <Nav.Link className="link" href="/discord">Discord</Nav.Link>
 
